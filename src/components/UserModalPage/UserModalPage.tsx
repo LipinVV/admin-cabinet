7ba6ef19a75691f1e6198d2c348039e2e5a92af0
LipinVV <vit.lipin@gmail.com>
2021-11-29 10:11:54 +0300

    styled task modal page

--- src/components/UserModalPage/UserModalPage.tsx
+++ src/components/UserModalPage/UserModalPage.tsx
@@ -1,8 +1,8 @@
 import React, {useContext, useState} from 'react';
 import {userCard} from '../../services/user';
-import './userModalPage.scss';
 import {ACTION, StoreContext} from '../../Storage/Storage';
 import {taskType} from '../../services/tasks';
+import './userModalPage.scss';
 
 type userModalPageProps = {
     userData: userCard,
@@ -10,28 +10,50 @@ type userModalPageProps = {
 }
 
 export const UserModalPage = ({userData, onClose}: userModalPageProps) => {
-    const { state, dispatch } = useContext(StoreContext);
+    const {state, dispatch} = useContext(StoreContext);
     const tasksByUsers = state.tasks.filter(task => task.userId === userData.id);
     const [task, setTask] = useState<taskType>({name: '', description: '', userId: 0});
 
-
     return (
-        <div className='userModalPage'>
-            <button onClick={onClose}>Закрыть</button>
-            <div>
-                <input onChange={(event) => setTask({...task, name: event.target.value, userId: userData.id})}/>Task
-                <input onChange={(event) => setTask({...task, description: event.target.value,  userId: userData.id})}/>Description
-                <button onClick={() => dispatch({action: ACTION.SET_TASK, data: task})}>Create</button>
-            </div>
+        <div className='user-modal-page'>
+            <h1 className='user-modal-page__header'> Сотрудник: {userData.name}</h1>
+            <label className='user-modal-page__label'>Название задачи
+                <input
+                    value={task.name}
+                    className='user-modal-page__input'
+                    onChange={(event) => setTask({...task, name: event.target.value, userId: userData.id})}
+                />
+            </label>
+            <label className='user-modal-page__label'>Описание задачи
+                <textarea
+                    value={task.description}
+                    className='user-modal-page__text-area'
+                    onChange={(event) => setTask({...task, description: event.target.value, userId: userData.id})}
+                />
+            </label>
+            <button
+                disabled={task.name.length < 10 || task.description.length < 10}
+                className='user-modal-page__create-button'
+                onClick={() => {
+                    dispatch({action: ACTION.SET_TASK, data: task});
+                    setTask({name: '', description: '', userId: 0});
+                }}>Создать</button>
             {tasksByUsers.map(task => {
                 return (
-                    <div key={task.userId}>
-                        <span>{task.name}</span>
-                        {task.description}
+                    <div
+                        className='user-modal-page__tasks'
+                        key={task.name}
+                    >
+                        <h3>Поставленная задача:</h3>
+                        <span className='user-modal-page__task-name'>Название: {task.name}</span>
+                        <span className='user-modal-page__task-description'>Описание: {task.description}</span>
                     </div>
                 )
             })}
-            <div className='userModalPage__field'> userData: {userData.name}</div>
+            <button
+                className='user-modal-page__close-button'
+                onClick={onClose}>Закрыть
+            </button>
         </div>
     )
 }
